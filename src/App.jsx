be96af78e13a39df919498a4175cf46268d8b2c1
Rahat Bin Taleb <rahat3062@gmail.com>
2026-03-15 15:21:40 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,14 @@ function App() {
         </Router>
         <Toaster />
       </QueryClientProvider>
+      <style>{`
+        html {
+          scrollbar-width: none;
+        }
+        html::-webkit-scrollbar {
+          display: none;
+        }
+      `}</style>
     </AuthProvider>
   )
 }
