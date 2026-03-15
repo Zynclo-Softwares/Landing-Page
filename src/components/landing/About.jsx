6b2511105e3890d7093f13358e533d908408ed02
Rahat Bin Taleb <rahat3062@gmail.com>
2026-03-15 04:08:50 +0000

    File changes

--- src/components/landing/About.jsx
+++ src/components/landing/About.jsx
@@ -85,7 +85,7 @@ export default function About({ appIconUrl }) {
       {LOGOS.map((logo) => (
         <motion.div
           key={logo.name}
-          className="absolute pointer-events-none select-none hidden sm:block"
+          className="absolute pointer-events-none select-none"
           style={{ left: logo.x, top: logo.y, width: logo.size, height: logo.size, opacity: 0.14, willChange: "transform, opacity" }}
           animate={{ y: [0, -14, 0], rotate: [logo.rot, logo.rot + 5, logo.rot], opacity: [0.08, 0.18, 0.08] }}
           transition={{ duration: logo.dur, repeat: Infinity, ease: "easeInOut", delay: logo.delay }}
