47a5b6c5df3861b242aaf243f070644874ae0a1a
Rahat Bin Taleb <rahat3062@gmail.com>
2026-03-15 03:18:17 +0000

    File changes

--- src/components/landing/About.jsx
+++ src/components/landing/About.jsx
@@ -104,12 +104,12 @@ export default function About({ appIconUrl }) {
           className="text-center mb-16 relative"
         >
           {/* Astronaut decoration */}
-          <div className="absolute -right-8 -top-6 opacity-30" style={{ width: 140, height: 140 }}>
+          <div className="absolute -right-8 -top-16 opacity-30" style={{ width: 140, height: 140 }}>
             <img 
               src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69b591daf6da87ce28f820e7/9de7f6d2b_astronaut.png"
               alt="Astronaut"
               className="w-full h-full"
-              style={{ filter: "hue-rotate(-50deg) saturate(1.2) brightness(0.9)" }}
+              style={{ filter: "hue-rotate(-50deg) saturate(1.2) brightness(0.9)", transform: "rotate(90deg)" }}
             />
           </div>
         
